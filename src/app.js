2b52d8cf93400d4ef8932734ebb58df1a324d0e8
BYUNGMIN CHOI <byung803@gmail.com>
2018-10-28 17:03:44 +0900

    todoApp boilerplate first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import reducer from './reducers';
+import Router from './router';
+import './styles/styles.scss';
+import 'normalize.css/normalize.css';
+
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+
+ReactDOM.render(
+    <Provider store={store} >
+        <Router />
+    </Provider>
+    , document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = (env) => {
+    const isProduction = env === 'production';
+
+    return {
+        mode: 'development',
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public', 'dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: ExtractTextPlugin.extract({
+                    use: [{
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }]
+                })
+            }]
+        },
+        plugins: [
+            new ExtractTextPlugin('style.css')
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/dist/'
+        }
+    }
+}
