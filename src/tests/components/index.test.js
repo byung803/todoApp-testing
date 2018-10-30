fb0504c513eed92805762ab45f1e4dc74284ef3c
최병민(01045842518) <byung803@gmail.com>
2018-10-30 19:17:13 +0900

    delete action index.test.js snapshot & contents

--- src/tests/components/index.test.js
+++ src/tests/components/index.test.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Index from '../../components';
 
 test('should test components index', () => {
-    const wrapper = shallow(<Index />);
-    expect(wrapper).toMatchSnapshot();
+
 });
