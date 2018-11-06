01060d36cf0582649a968d3ad8ad2aad1d6d03d8
BYUNGMIN CHOI <byung803@gmail.com>
2018-11-06 15:32:22 +0900

    TodoList test added

--- src/components/__test__/TodoList.test.js
+++ src/components/__test__/TodoList.test.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import TodoList from '../TodoList';
+import { todosFixture } from './fixtures';
+
+let wrapper;
 
 
-describe('TodoList compo', () => {
-    it('', () => {
+describe('TodoList compo with empty todos', () => {
 
+    beforeEach(() => {
+        wrapper = shallow(<TodoList todos={[]} />);
+    });
+
+    it('should have a div', () => {
+        expect(wrapper.find('div').length).toBe(1);
     })
+
+    it('should return nothing when got empty todo list', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+});
+
+describe('TodoList compo with fixture todos', () => {
+
+    beforeEach(() => {
+        wrapper = shallow(<TodoList todos={todosFixture} />);
+    });
+
+    it('should return Todo Components when got todosFixture todo list', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
 });
---
+++ src/components/__test__/fixtures/index.js
@@ -0,0 +1,17 @@
+export const todosFixture = [
+    {
+        id: 1,
+        text: 'first',
+        completed: false
+    },
+    {
+        id: 2,
+        text: 'second',
+        completed: true
+    },
+    {
+        id: 3,
+        text: 'third',
+        completed: false
+    }
+]
